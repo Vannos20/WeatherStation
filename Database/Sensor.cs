696860499c96c26e4696e3ca2a80393b626cc540
uczen <uczen@IGOR>
2025-11-14 15:25:20 +0100

    uzupelnienie proj

---
+++ Database/Sensor.cs
@@ -0,0 +1,12 @@
+﻿namespace Database
+{
+    public class Sensor
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime MeasuremantDate { get; set; }
+    }
+}
