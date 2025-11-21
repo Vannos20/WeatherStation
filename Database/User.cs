3ba94321c07476776c5b40c1c2832e675a337783
SMyk <75905145+Vannos20@users.noreply.github.com>
2025-11-21 15:43:48 +0100

    Refactor User class to include constructor and private Name

--- Database/User.cs
+++ Database/User.cs
@@ -1,9 +1,16 @@
+
 ﻿namespace Database
 {
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-
+        string Name { get; set; }
+        private static int counter = 0;
+        public User(string name, int id)
+        {
+            this.Id = id;
+            this.Name = name;
+            counter++;
+        }
     }
 }
