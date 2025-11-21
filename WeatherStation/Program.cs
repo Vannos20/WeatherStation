7457f265f8ea9c969008fea3020162759cf13359
SMyk <75905145+Vannos20@users.noreply.github.com>
2025-11-21 15:47:51 +0100

    Update Program.cs
    Comment out unused code for clarity.

--- WeatherStation/Program.cs
+++ WeatherStation/Program.cs
@@ -6,8 +6,8 @@ class Program
 {
     public static void Main(string[] args)
     {
-        //   Console.WriteLine("Hello, World!");
-        // User user = new User();
+        //Console.WriteLine("Hello, World!");
+        //User user = new User();
         //Sensor sencor = new Sencor();
         User user1 = new User("Ja", 999);
 
