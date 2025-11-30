4807b66938627e0d7d706ab62c72017e1600d755
SMyk <75905145+Vannos20@users.noreply.github.com>
2025-11-30 12:34:57 +0100

    Update Program.cs

--- WeatherStation/Program.cs
+++ WeatherStation/Program.cs
@@ -1,16 +1,12 @@
-﻿using Database;
+using Database;
 
 namespace WeatherStation;
 
-class Program
+public class Program
 {
     public static void Main(string[] args)
     {
-        //Console.WriteLine("Hello, World!");
-        //User user = new User();
-        //Sensor sencor = new Sencor();
         User user1 = new User("Ja", 999);
-
         User user2 = new User("Ja", 999);
         Sensor sensor = new Sensor();
     }
