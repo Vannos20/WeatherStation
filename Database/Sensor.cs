a1cee4dcd0d63851bb55615f7613208acce2c83d
uczen <uczen@bull>
2026-01-30 15:57:39 +0100

    dodano dane do baz

--- Database/Sensor.cs
+++ Database/Sensor.cs
@@ -4,7 +4,7 @@ public class Sensor
 {
 
     public int Id { get; set; }
-    public string Type { get; set; }
+    public string? Type { get; set; }
     public string Name { get; set; }
     public DateTime MeasurementDate { get; set; }
     
